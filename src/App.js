6398c4397a490db045d69a235e9c551d4453a936
Reydel Ocon <reydel321@gmail.com>
2021-11-26 22:15:29 +0800

    finalizing my todo list app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './style.css'
+
+import plusIcon from './assets/plus.svg'
+import deleteIcon from './assets/delete.svg'
+
+import { faTrash, faEdit, faPaperPlane, faPlusCircle, faBookDead } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const App = () => {
 
     const [todos, setTodos] = useState([])
     const [todo, setTodo] = useState('')
+    const [edit, setEdit] = useState(null)
+    const [editText, setEditText] = useState('')
+
+
+ 
+
+
+    useEffect(() => {
+        const localPreHolder = localStorage.getItem('data')
+        const localSavedTodos = JSON.parse(localPreHolder)
+
+        if(localSavedTodos){
+            setTodos(localSavedTodos)
+        }
+    }, [])
+
+
+    useEffect(()=> {
+        const localHolder = JSON.stringify(todos)
+        localStorage.setItem('data', localHolder)
+    }, [todos])
+
 
     function submitHandler(y) {
         y.preventDefault()
@@ -12,6 +42,12 @@ const App = () => {
             id: new Date().getTime(),
             title: todo,
             status: false,
+        }
+
+        if(!(todo === '')){
+            setTodo()
+        } else {
+            return todo
         }
 
         setTodos([...todos.concat(saveTodo)])
@@ -24,19 +60,73 @@ const App = () => {
         setTodos(newTodo)
     }
 
+    function update(id){
+
+        const updateTodo = [...todos].map((bago) => {
+            if(bago.id === id){
+                bago.status = !bago.status
+            }
+            return bago
+        })
+
+        setTodos(updateTodo)
+    }
+
+    
+
+
+    function editSubmit(id){
+        const updateTodoss = [...todos].map((bago) => {
+            if(bago.id === id){
+                bago.title = editText
+            }
+
+            return bago
+        })
+        setTodos(updateTodoss)
+        setEdit([])
+        setEditText('')
+
+    }
+
     return ( 
         <div className="main">
-           <form onSubmit={submitHandler}>
-               <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo}/>
-            
-               <button type='submit'>Add</button>
-           </form>
-           {todos.map((hey) => 
-           <div key={hey.id}>
-               <div>{hey.title}</div>
-               <button onClick={() => remove(hey.id)}
-                >Delete</button>
-            </div>)}
+            <div className='main__holder'>
+                <form onSubmit={submitHandler}>
+                    <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo}/>
+                    
+                    <button type='submit' className='plus__holder'>
+                        <FontAwesomeIcon className='plus' icon={faPlusCircle}/>
+                    </button>
+                </form>
+                <div className='main__body'>
+                {todos.map((hey) =>
+                 <div className={hey.status === true ? 'todoDone' : 'todoShit'} key={hey.id}>
+                    {edit === hey.id ? 
+                    (<input type='text' onChange={(e) => setEditText(e.target.value)} value={editText}>
+                    </input>) 
+                    : 
+                    (<div className='todo__title'>{hey.title}</div>)}
+
+                    <div className='todos__feature'>
+                        <input type='checkbox' onChange={() => update(hey.id)} checked={hey.status}>
+                        </input>
+                        <button onClick={() => setEdit(hey.id)}>
+                            <FontAwesomeIcon className='edit' icon={faEdit}/>
+                        </button>
+                            {edit === hey.id ?
+                        (<button onClick={() => editSubmit(hey.id)}>
+                            <FontAwesomeIcon className='submit' icon={faPaperPlane}/>
+                        </button>)
+                             : 
+                        (<button onClick={() => remove(hey.id)}>
+                         <FontAwesomeIcon className='trash' icon={faTrash}/>
+                         </button>)} 
+                     </div>
+
+                </div>)}
+                </div>
+            </div>
         </div>
      );
 }
